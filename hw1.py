626f1cc5606b7f60462e259e58625919e8648b30
heewon2e <163503613+heewon2e@users.noreply.github.com>
2024-03-22 17:53:53 +0900

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -7,19 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1aNirVghUtiqOsEiZJNahnL4xEpA8pVEx
 """
 
-def get_radius(prompt):
-  r = int(input(prompt))
-  return r
-def get_circle_area(radius):
-  area = 3.14 * radius * radius
-  return area
-
-input_r = ('넓이를  구하고자 하는 원의 반지름은? ' )
-result = get_radius(input_r)
-print(result)
-
-area_r = get_circle_area(result)
-print("반지름이", result,"인 원의 넓이 = ",3.14*result*result,"=",area_r)
 
 def get_radius(prompt):
     r = int(input(prompt))
